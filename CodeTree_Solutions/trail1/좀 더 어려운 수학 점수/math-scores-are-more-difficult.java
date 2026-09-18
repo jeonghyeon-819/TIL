8c4e4fd0ac0adb11a33e5ce00f6f4e93b164c3e5
jeonghyeon-819 <jeonghyeon819@gmail.com>
2026-09-19 00:08:03 +0900

    [Passed] 좀 더 어려운 수학 점수 (136ms, 10MB)

---
+++ CodeTree_Solutions/trail1/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int Am = sc.nextInt();
+        int Ae = sc.nextInt();
+        int Bm = sc.nextInt();
+        int Be = sc.nextInt();
+
+        if (Am > Bm) {
+            System.out.print("A");
+        } else if ((Am == Bm) && (Ae > Be)) {
+            System.out.print("A");
+        } else {
+            System.out.print("B");
+        }
+
+    }
+}
