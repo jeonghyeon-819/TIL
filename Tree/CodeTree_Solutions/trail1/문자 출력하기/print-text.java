a9ca46cd61b8965c99321f947a6f1091d1e54653
jeonghyeon-819 <jeonghyeon819@gmail.com>
2026-09-22 03:51:22 +0900

    [Passed] 문자 출력하기 (121ms, 10MB)

---
+++ Tree/CodeTree_Solutions/trail1/문자 출력하기/print-text.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.next();
+
+        for (int i = 0; i < 8; i++) {
+            System.out.print(text);
+        }
+
+    }
+}
